efcc20b71066677edea86f60f03f000319444e3b
Can KURT <52429586+cankurt17@users.noreply.github.com>
2022-01-08 15:28:05 +0300

    Add files via upload

---
+++ stream_builder_extensions.dart
@@ -0,0 +1,25 @@
+extension StreamExtension<T> on Stream {
+  Widget toBuild<U>(
+      {required dynamic Function(T data) onSucces, Widget? isEmpty}) {
+    return StreamBuilder<dynamic>(
+      stream: this,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data is List) {
+            if (snapshot.data.isEmpty) return _isEmpty(isEmpty, snapshot);
+          }
+          return onSucces(snapshot.data);
+        } else {
+          return _centerLoading;
+        }
+      },
+    );
+  }
+
+  Center get _centerLoading => const Center(child: CustomProgresIndicator());
+
+  Widget _isEmpty(Widget? isEmpty, AsyncSnapshot<dynamic> snapshot) {
+    if (isEmpty != null) return isEmpty;
+    return const Center(child: Text("Null"));
+  }
+}
