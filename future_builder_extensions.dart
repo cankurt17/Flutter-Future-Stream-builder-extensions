634dff5987468edf93621a39235be2eeae199ad2
Can KURT <52429586+cankurt17@users.noreply.github.com>
2022-01-08 15:25:42 +0300

    Add files via upload

---
+++ future_builder_extensions.dart
@@ -0,0 +1,26 @@
+ 
+extension FutureExtension<T> on Future {
+  Widget toBuild<U>(
+      {required dynamic Function(T data) onSucces, Widget? isEmpty}) {
+    return FutureBuilder<dynamic>(
+      future: this,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data is List) {
+            if (snapshot.data.isEmpty) return _isEmpty(isEmpty, snapshot);
+          }
+          return onSucces(snapshot.data);
+        } else {
+          return _centerLoading;
+        }
+      },
+    );
+  }
+
+  Center get _centerLoading => const Center(child: CustomProgresIndicator());
+
+  Widget _isEmpty(Widget? isEmpty, AsyncSnapshot<dynamic> snapshot) {
+    if (isEmpty != null) return isEmpty;
+    return const Center(child: Text("Veri Yok"));
+  }
+}
